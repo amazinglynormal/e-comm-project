305fe1eab6d2e962011704b4f7446a29e66cbf07
Stephen <stephenbrady365@gmail.com>
2021-06-21 22:02:01 +0100

    Add dependencies for testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     "plugin:jsx-a11y/recommended",
     "prettier",
     "plugin:prettier/recommended",
+    "plugin:jest-dom/recommended",
   ],
   rules: {
     "no-unused-vars": "off",
