4c30df8a65715b142a46583c2a61e8852de9cc5f
Stephen <stephenbrady365@gmail.com>
2021-09-18 10:42:52 +0100

    Add CollectionSelection to home page. Closes #3.

--- src/frontend/src/home/Home.tsx
+++ src/frontend/src/home/Home.tsx
@@ -1,9 +1,11 @@
 import { PromoSection } from "./PromoSection";
+import { CollectionSelection } from "./CollectionSelection";
 
 const Home = () => {
   return (
     <div>
       <PromoSection />
+      <CollectionSelection />
     </div>
   );
 };
