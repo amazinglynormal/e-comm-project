2b6edc9ceefa870b79e620641c2444856703d2b3
Stephen <stephenbrady365@gmail.com>
2021-09-18 08:48:31 +0100

    Add and set up router. Closes #4.

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./home/Home";
+
 import "tailwindcss/tailwind.css";
 
 const App = () => {
-  return <h1 className="text-5xl">Hello World</h1>;
+  return (
+    <Router>
+      <h1 className="text-2xl">Hello World</h1>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
