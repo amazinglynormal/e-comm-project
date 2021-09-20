eea628bdad742a6427b88636ffd55e47872c7ef3
Stephen <stephenbrady365@gmail.com>
2021-09-20 17:01:01 +0100

    Rename routes

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,25 +1,25 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { StoreNavigation } from "./components/StoreNavigation";
+import { StoreNavigation } from "./navigation/StoreNavigation";
 import { Footer } from "./components/Footer";
 
 import Home from "./home/Home";
 import SignUp from "./signup/SignUp";
 import LogIn from "./login/LogIn";
 import ProductDetails from "./product/ProductDetails";
+import Products from "./product/Products";
 
 import "tailwindcss/tailwind.css";
-import Products from "./product/Products";
 
 const App = () => {
   return (
     <Router>
       <StoreNavigation />
       <Switch>
-        <Route path="/product/:id">
+        <Route path="/products/:id">
           <ProductDetails />
         </Route>
-        <Route path="/product">
+        <Route path="/products">
           <Products />
         </Route>
         <Route path="/login">
