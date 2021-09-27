35f69d04dbefd51fdcaf3f6fcd263e6ffe93162f
Stephen <stephenbrady365@gmail.com>
2021-09-27 09:19:32 +0100

    Add custom queries to ProductRepository

--- src/main/java/sb/ecomm/product/ProductRepository.java
+++ src/main/java/sb/ecomm/product/ProductRepository.java
@@ -3,6 +3,11 @@ package sb.ecomm.product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findByName(String name);
+    List<Product> findByNameLikeOrFeaturesContaining(String name, String
+                                                     features);
 }
