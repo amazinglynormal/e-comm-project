43be5ca47b108a8794de889a0cdbe9d330917742
Stephen <stephenbrady365@gmail.com>
2021-09-27 09:08:52 +0100

    Add CRUD functionality for Category endpoints. Closes #25.

--- src/main/java/sb/ecomm/category/CategoryController.java
+++ src/main/java/sb/ecomm/category/CategoryController.java
@@ -1,11 +1,45 @@
 package sb.ecomm.category;
 
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
-@Service
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/v1/categories")
 public class CategoryController {
 
-    private final CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public Iterable<Category> getAllCategories() {
+        return categoryService.findAllCategories();
+    }
+
+    @PostMapping
+    public Category addNewCategory(@RequestBody Category newCategory) {
+        return categoryService.addNewCategory(newCategory);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Category> findCategoryById(@PathVariable long id) {
+        return categoryService.findCategoryById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable long id,
+                                             @RequestBody Category updatedCategory) {
+        return categoryService.updateCategory(id, updatedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable long id) {
+        categoryService.deleteCategoryById(id);
+    }
 }
--- src/main/java/sb/ecomm/category/CategoryService.java
+++ src/main/java/sb/ecomm/category/CategoryService.java
@@ -1,11 +1,50 @@
 package sb.ecomm.category;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
+import java.util.Optional;
+
+
 @Service
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    Iterable<Category> findAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    Category addNewCategory(Category newCategory) {
+        return categoryRepository.save(newCategory);
+    }
+
+    Optional<Category> findCategoryById(Long id) {
+       Optional<Category> category = categoryRepository.findById(id);
+       if (category.isPresent()) {
+           return category;
+       } else {
+           throw new RuntimeException();
+       }
+    }
+
+    Category updateCategory(Long id, Category updatedCategory) {
+        Optional<Category> category = categoryRepository.findById(id);
+        if (category.isPresent()) {
+            category.get().setName(updatedCategory.getName());
+            category.get().setDescription(updatedCategory.getDescription());
+            return categoryRepository.save(category.get());
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+    void deleteCategoryById(Long id) {
+        categoryRepository.deleteById(id);
+    }
 }
