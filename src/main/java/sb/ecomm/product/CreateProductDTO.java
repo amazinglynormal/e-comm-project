90de552f5d37db20b67fad9f03ccd448ff3e3468
Stephen <stephenbrady365@gmail.com>
2021-09-28 20:50:31 +0100

    Add Product DTOs

---
+++ src/main/java/sb/ecomm/product/CreateProductDTO.java
@@ -0,0 +1,27 @@
+package sb.ecomm.product;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateProductDTO {
+
+    private String name;
+
+    private String description;
+
+    private String features;
+
+    private double priceCAD;
+
+    private double priceUSD;
+
+    private double priceAUD;
+
+    private double priceEUR;
+
+    private double priceGBP;
+
+    private Long categoryId;
+}
