97bb3db95c12451e1bc4e293ec9c2ac7e41e51f8
Stephen <stephenbrady365@gmail.com>
2021-10-08 20:15:56 +0100

    Add Role enum that implements GrantedAuthority

---
+++ src/main/java/sb/ecomm/user/Role.java
@@ -0,0 +1,13 @@
+package sb.ecomm.user;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_ADMIN,
+    ROLE_CUSTOMER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
