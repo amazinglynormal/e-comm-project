bb7ee4168c7fffc9f8fd274235f83fbd5cfc153e
Stephen <stephenbrady365@gmail.com>
2021-10-09 09:27:46 +0100

    Associate authorities with each role

--- src/main/java/sb/ecomm/user/Role.java
+++ src/main/java/sb/ecomm/user/Role.java
@@ -1,6 +1,24 @@
 package sb.ecomm.user;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static sb.ecomm.user.Authorities.*;
+
 public enum Role {
-    CUSTOMER,
-    ADMIN
+    CUSTOMER(Sets.newHashSet(PRODUCT_READ, ORDER_READ, ORDER_WRITE, USER_READ
+            , USER_WRITE, CATEGORY_READ)),
+    ADMIN(Sets.newHashSet(PRODUCT_READ, PRODUCT_WRITE, ORDER_READ,
+            ORDER_WRITE, CATEGORY_READ, CATEGORY_WRITE, USER_READ, USER_WRITE));
+
+    private final Set<Authorities> authorities;
+
+    Role(Set<Authorities> authorities) {
+        this.authorities = authorities;
+    }
+
+    public Set<Authorities> getAuthorities() {
+        return authorities;
+    }
 }
