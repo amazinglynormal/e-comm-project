6600774f56aa0ee631bdcc0af65a6b43dba5d5f4
Stephen <stephenbrady365@gmail.com>
2021-10-14 09:00:18 +0100

    Annotate UserDetailsServiceImpl

--- src/main/java/sb/ecomm/security/UserDetailsServiceImpl.java
+++ src/main/java/sb/ecomm/security/UserDetailsServiceImpl.java
@@ -1,11 +1,11 @@
 package sb.ecomm.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import sb.ecomm.user.User;
 import sb.ecomm.user.UserRepository;
 
@@ -13,8 +13,10 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
+
 
     public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -22,9 +24,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
         User user =
                 userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
+
 
         return new UserDetailsImpl(user.getId(),
                 user.getEmail(), user.getPassword(), getAuthorities(user),
