d4d311845f60a8084f9f42b258b718244c65142e
Stephen <stephenbrady365@gmail.com>
2021-10-19 08:43:33 +0100

    Connect sign up form to backend

--- src/frontend/src/user/SignUp.tsx
+++ src/frontend/src/user/SignUp.tsx
@@ -1,6 +1,67 @@
+import axios from "axios";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
+import User from "../interfaces/user.interface";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onFormChange = (event: ChangeEvent<HTMLInputElement>) => {
+    switch (event.target.id) {
+      case "name":
+        setFormData((data) => {
+          return {
+            ...data,
+            name: event.target.value,
+          };
+        });
+        break;
+      case "email":
+        setFormData((data) => {
+          return {
+            ...data,
+            email: event.target.value,
+          };
+        });
+        break;
+      case "password":
+        setFormData((data) => {
+          return {
+            ...data,
+            password: event.target.value,
+          };
+        });
+        break;
+      default:
+        return;
+    }
+  };
+
+  const sendSignUpForm = async (
+    name: string,
+    email: string,
+    password: string
+  ) => {
+    const response = await axios.post<User>("/api/v1/users", {
+      username: name,
+      email,
+      password,
+    });
+    return response.data;
+  };
+
+  const onFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { name, email, password } = formData;
+    if (name.length > 0 && email.length > 0 && password.length > 8) {
+      sendSignUpForm(name, email, password).then((res) => console.log(res));
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -16,7 +77,26 @@ const SignUp = () => {
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" onSubmit={onFormSubmit}>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  value={formData.name}
+                  onChange={onFormChange}
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
             <div>
               <label
                 htmlFor="email"
@@ -29,6 +109,8 @@ const SignUp = () => {
                   id="email"
                   name="email"
                   type="email"
+                  value={formData.email}
+                  onChange={onFormChange}
                   autoComplete="email"
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -48,6 +130,8 @@ const SignUp = () => {
                   id="password"
                   name="password"
                   type="password"
+                  value={formData.password}
+                  onChange={onFormChange}
                   autoComplete="current-password"
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
