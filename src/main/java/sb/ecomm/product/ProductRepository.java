9da67419daf9007da1ad68e25705da571aa9d55b
Stephen <stephenbrady365@gmail.com>
2021-10-29 19:26:22 +0100

    Change ProductRepository to extend PagingAndSortingRepository interface

--- src/main/java/sb/ecomm/product/ProductRepository.java
+++ src/main/java/sb/ecomm/product/ProductRepository.java
@@ -1,11 +1,15 @@
 package sb.ecomm.product;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+import sb.ecomm.category.Category;
 
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
     List<Product> findByNameContainingIgnoreCase(String name);
+    Page<Product> findProductByCategory(Category category, Pageable pageable);
 }
