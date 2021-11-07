d03a855099ad2596d3432d766784acd9166c42cd
Stephen <stephenbrady365@gmail.com>
2021-11-07 12:58:04 +0000

    Change anchor element to Link component

--- src/frontend/src/product/ProductsListItem.tsx
+++ src/frontend/src/product/ProductsListItem.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useCurrency } from "../state/CurrencyContext";
 import Product from "../interfaces/product.interface";
 
@@ -16,7 +17,7 @@ const ProductsListItem = ({ product }: Props) => {
 
   return (
     <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-      <a key={product.id} href={`/products/${product.id}`} className="group">
+      <Link key={product.id} to={`/products/${product.id}`} className="group">
         <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
           <img
             src={product.imageSrc}
@@ -28,7 +29,7 @@ const ProductsListItem = ({ product }: Props) => {
         <p className="mt-1 text-lg font-medium text-gray-900">
           {`${currencySymbol[currency]}${product[currency]}`}
         </p>
-      </a>
+      </Link>
     </div>
   );
 };
