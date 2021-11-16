017a3ef345ba68756a629475bb1695a9b8c6654e
Stephen <stephenbrady365@gmail.com>
2021-11-16 20:58:34 +0000

    Alter interface property from status to orderStatus

--- src/frontend/src/interfaces/order.interface.ts
+++ src/frontend/src/interfaces/order.interface.ts
@@ -2,7 +2,7 @@ import Product from "../types/Product.type";
 
 interface Order {
   id: number;
-  status: "INACTIVE" | "ACTIVE" | "DISPATCHED" | "DELIVERED";
+  orderStatus: "INACTIVE" | "ACTIVE" | "DISPATCHED" | "DELIVERED";
   userId: string;
   products: Product[];
 }
