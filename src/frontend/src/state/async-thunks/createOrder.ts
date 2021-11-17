1d893e7909675abc1e7f230c433c4f397cff592f
Stephen <stephenbrady365@gmail.com>
2021-11-17 08:40:42 +0000

    Fix arg type for createOrder async-thunk

--- src/frontend/src/state/async-thunks/createOrder.ts
+++ src/frontend/src/state/async-thunks/createOrder.ts
@@ -1,19 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import Product from "../../types/Product.type";
 import { RootState } from "../store";
 import Order from "../../interfaces/order.interface";
 
-const createOrder = createAsyncThunk<Order, Product, { state: RootState }>(
+const createOrder = createAsyncThunk<Order, number, { state: RootState }>(
   "order/createOrder",
-  async (product: Product, { getState }) => {
+  async (productId: number, { getState }) => {
     const userId = getState().user.id;
     const token = getState().user.token;
     const response = await axios.post<Order>(
       `/api/v1/users/${userId}/orders`,
       {
         userId,
-        productIds: [product.id],
+        productIds: [productId],
         orderStatus: "ACTIVE",
       },
       {
