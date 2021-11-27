2157b8559492a37e85848b4c4a19fdb3b2b717e4
Stephen <stephenbrady365@gmail.com>
2021-11-27 19:40:36 +0000

    Add FilterCheckbox component

---
+++ src/frontend/src/product/FilterCheckbox.tsx
@@ -0,0 +1,45 @@
+import { ChangeEvent, useState } from "react";
+import FilterForm from "../interfaces/filterForm.interface";
+
+interface Props {
+  category: { id: "categories" | "colors" | "sizes"; name: string };
+  option: { value: number; label: string };
+  formChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+  filterForm: FilterForm;
+}
+
+const FilterCheckbox = ({
+  category,
+  option,
+  formChangeHandler,
+  filterForm,
+}: Props) => {
+  const [checked, setChecked] = useState(
+    filterForm[category.id].includes(option.value)
+  );
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (checked) setChecked(false);
+    else setChecked(true);
+
+    formChangeHandler(event);
+  };
+
+  return (
+    <div className="flex items-center">
+      <input
+        id={`${category.id}-${option.value}`}
+        name={`${category.id}`}
+        value={option.value}
+        type="checkbox"
+        className="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
+        onChange={onChange}
+      />
+      <label htmlFor={`${category.id}`} className="ml-3 text-sm text-gray-600">
+        {option.label}
+      </label>
+    </div>
+  );
+};
+
+export default FilterCheckbox;
