1176c19ec679fd7a79513ffcda6aa3fd219f3e4d
Stephen <stephenbrady365@gmail.com>
2021-12-03 08:26:36 +0000

    Add Optional return type to custom query

--- src/main/java/sb/ecomm/category/CategoryRepository.java
+++ src/main/java/sb/ecomm/category/CategoryRepository.java
@@ -3,7 +3,9 @@ package sb.ecomm.category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-    Category findByName(String name);
+    Optional<Category> findByName(String name);
 }
