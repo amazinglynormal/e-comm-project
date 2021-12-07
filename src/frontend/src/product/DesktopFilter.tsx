51b2689e2d2e5a2dd058b43fef6c4a6a61adcca6
Stephen <stephenbrady365@gmail.com>
2021-12-07 16:12:48 +0000

    Add FilterCheckbox to desktop filter

--- src/frontend/src/product/DesktopFilter.tsx
+++ src/frontend/src/product/DesktopFilter.tsx
@@ -1,36 +1,37 @@
+import { ChangeEvent } from "react";
 import FilterOptions from "../interfaces/filterOptions.interface";
+import FilterForm from "../interfaces/filterForm.interface";
+import FilterCheckbox from "./FilterCheckbox";
 
 interface Props {
   filters: FilterOptions[];
+  formChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+  filterForm: FilterForm;
 }
 
-export const DesktopFilter = ({ filters }: Props) => {
+export const DesktopFilter = ({
+  filters,
+  formChangeHandler,
+  filterForm,
+}: Props) => {
   return (
     <div className="hidden lg:block">
       <form className="divide-y divide-gray-200 space-y-10">
-        {filters.map((section, sectionIdx) => (
-          <div key={section.name} className={sectionIdx === 0 ? "" : "pt-10"}>
+        {filters.map((category, categoryIdx) => (
+          <div key={category.name} className={categoryIdx === 0 ? "" : "pt-10"}>
             <fieldset>
               <legend className="block text-sm font-medium text-gray-900">
-                {section.name}
+                {category.name}
               </legend>
               <div className="pt-6 space-y-3">
-                {section.options.map((option, optionIdx) => (
-                  <div key={option.value} className="flex items-center">
-                    <input
-                      id={`${section.id}-${optionIdx}`}
-                      name={`${section.id}[]`}
-                      defaultValue={option.value}
-                      type="checkbox"
-                      className="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
-                    />
-                    <label
-                      htmlFor={`${section.id}-${optionIdx}`}
-                      className="ml-3 text-sm text-gray-600"
-                    >
-                      {option.label}
-                    </label>
-                  </div>
+                {category.options.map((option) => (
+                  <FilterCheckbox
+                    key={option.value}
+                    category={category}
+                    option={option}
+                    formChangeHandler={formChangeHandler}
+                    filterForm={filterForm}
+                  />
                 ))}
               </div>
             </fieldset>
