5b8ec0ac2b1c4758b037e4764726d36a1826bfd4
Stephen <stephenbrady365@gmail.com>
2021-12-07 16:10:43 +0000

    Refactor fetchProducts  to include filter options

--- src/frontend/src/hooks/useProductSWR.ts
+++ src/frontend/src/hooks/useProductSWR.ts
@@ -1,15 +1,30 @@
 import axios from "axios";
 import useSWR from "swr";
 import Product from "../types/Product.type";
+import qs from "qs";
 
 const productsFetcher = async (url: string) => {
   const res = await axios.get(url);
   return res.data;
 };
 
-export default function useProductsSWR(categoryId: number, page: number) {
+export default function useProductsSWR(
+  categories: string[],
+  colors: string[],
+  sizes: string[],
+  page: number
+) {
+  categories.sort();
+  colors.sort();
+  sizes.sort();
+
+  const queryString = qs.stringify(
+    { categories, colors, sizes },
+    { arrayFormat: "comma" }
+  );
+
   const { data, error } = useSWR(
-    `/api/v1/products?cat=${categoryId}&page=${page}`,
+    `/api/v1/products?${queryString}&page=${page}`,
     productsFetcher
   );
 
