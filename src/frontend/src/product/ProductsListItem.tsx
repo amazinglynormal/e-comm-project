5982fa542db9c8424fef2326dc07020da09f746c
Stephen <stephenbrady365@gmail.com>
2021-12-08 15:45:09 +0000

    Change base link url

--- src/frontend/src/product/ProductsListItem.tsx
+++ src/frontend/src/product/ProductsListItem.tsx
@@ -16,7 +16,11 @@ const ProductsListItem = ({ product }: Props) => {
   const { currency } = useCurrency();
 
   return (
-    <Link key={product.id} to={`/products/${product.id}`} className="group">
+    <Link
+      key={product.id}
+      to={`/product/details/${product.id}`}
+      className="group"
+    >
       <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
         <img
           src={product.imageSrc}
