6fcb6f5ae03a59a5f8d4d0bc9139006f1c40bc17
Stephen <stephenbrady365@gmail.com>
2021-12-13 09:57:21 +0000

    Refactor PageSelect to not show extra unneeded page

--- src/frontend/src/product/ListPageSelect.tsx
+++ src/frontend/src/product/ListPageSelect.tsx
@@ -43,7 +43,7 @@ const ListPageSelect = ({
         >
           {page}
         </button>
-        {currentPage < totalPages && (
+        {currentPage < totalPages - 1 && (
           <button
             className="inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25"
             onClick={() => pageChangeHandler(currentPage + 1)}
