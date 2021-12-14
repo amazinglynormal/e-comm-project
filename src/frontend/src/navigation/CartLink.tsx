ed9172d2390af11d1b98200cc51a15f4bd02864c
Stephen <stephenbrady365@gmail.com>
2021-12-14 20:51:05 +0000

    Refactor link to include number of items in bag

--- src/frontend/src/navigation/CartLink.tsx
+++ src/frontend/src/navigation/CartLink.tsx
@@ -1,16 +1,20 @@
 import { ShoppingBagIcon } from "@heroicons/react/outline";
 import { Link } from "react-router-dom";
+import { useAppSelector } from "../hooks/redux-hooks";
+import { selectOrderProducts } from "../state/orderSlice";
 
 export const CartLink = () => {
+  const productsInCart = useAppSelector(selectOrderProducts);
+
   return (
     <div className="ml-4 flow-root lg:ml-8">
-      <Link to="/cart" className="group -m-2 p-2 flex items-center">
+      <Link to="/cart/1" className="group -m-2 p-2 flex items-center">
         <ShoppingBagIcon
           className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
           aria-hidden="true"
         />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-          0
+          {productsInCart === undefined ? 0 : productsInCart.length}
         </span>
         <span className="sr-only">items in cart, view bag</span>
       </Link>
