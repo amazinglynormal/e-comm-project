a3c70733fba65ba9949e95f0563fe1f7cd747db5
Stephen <stephenbrady365@gmail.com>
2021-12-22 09:04:28 +0000

    Disable add-to-cart button if not logged in

--- src/frontend/src/order/AddToCartButton.tsx
+++ src/frontend/src/order/AddToCartButton.tsx
@@ -2,6 +2,7 @@ import { useAppSelector, useAppDispatch } from "../hooks/redux-hooks";
 import createOrder from "../state/async-thunks/createOrder";
 import updateOrder from "../state/async-thunks/updateOrder";
 import { selectOrder } from "../state/orderSlice";
+import { selectUser } from "../state/userSlice";
 
 interface Props {
   productId: number;
@@ -10,6 +11,8 @@ interface Props {
 const AddToCartButton = ({ productId }: Props) => {
   const activeOrder = useAppSelector(selectOrder);
   const dispatch = useAppDispatch();
+
+  const { role } = useAppSelector(selectUser);
 
   const onClickHandler = () => {
     if (!activeOrder) {
@@ -24,6 +27,7 @@ const AddToCartButton = ({ productId }: Props) => {
       type="button"
       className="w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
       onClick={onClickHandler}
+      disabled={role === "GUEST"}
     >
       Add to bag
     </button>
