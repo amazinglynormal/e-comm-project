09f438eba4fa3628cb0b4624fae08ba8640d6fc0
Stephen <stephenbrady365@gmail.com>
2021-12-28 15:21:34 +0000

    Add methods for user order CRUD operations

--- src/main/java/sb/ecomm/user/UserService.java
+++ src/main/java/sb/ecomm/user/UserService.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import sb.ecomm.exceptions.UserNotFoundException;
+import sb.ecomm.order.OrderService;
+import sb.ecomm.order.dto.CreateOrderDTO;
+import sb.ecomm.order.dto.OrderDTO;
+import sb.ecomm.order.dto.UpdateOrderDTO;
 import sb.ecomm.user.dto.CreateUserDTO;
 import sb.ecomm.user.dto.UserDTO;
 import sb.ecomm.user.dto.UpdateUserDTO;
@@ -15,13 +19,17 @@ import java.util.UUID;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final OrderService orderService;
     private final PasswordEncoder passwordEncoder;
-    private ModelMapper mapper;
+    private final ModelMapper mapper;
 
     @Autowired
     public UserService(UserRepository userRepository,
-                       PasswordEncoder passwordEncoder, ModelMapper mapper) {
+                       OrderService orderService,
+                       PasswordEncoder passwordEncoder,
+                       ModelMapper mapper) {
         this.userRepository = userRepository;
+        this.orderService = orderService;
         this.passwordEncoder = passwordEncoder;
         this.mapper = mapper;
     }
@@ -55,6 +63,38 @@ public class UserService {
 
     void deleteUserAccount(UUID id) {
         userRepository.deleteById(id);
+    }
+
+    OrderDTO findUserOrderById(UUID userId,
+                               Long orderId) {
+        OrderDTO order = orderService.findOrderById(orderId);
+        if (!order.getUserId().equals(userId)) {
+            throw new RuntimeException("forbidden yeah");
+        }
+        return order;
+    }
+
+    OrderDTO addNewUserOrder(CreateOrderDTO createOrderDTO, UUID id) {
+         return orderService.addNewOrder(createOrderDTO, id);
+    }
+
+    OrderDTO updateUserOrder(UUID userId,
+                             Long orderId,
+                             UpdateOrderDTO updateOrderDTO) {
+        OrderDTO order = orderService.findOrderById(orderId);
+        if (!order.getUserId().equals(userId)) {
+            throw new RuntimeException("forbidden yeah");
+        }
+        return orderService.updateOrder(orderId, updateOrderDTO);
+    }
+
+    void deleteUserOrder(UUID userId, Long orderId) {
+        OrderDTO order = orderService.findOrderById(orderId);
+        if (!order.getUserId().equals(userId)) {
+            throw new RuntimeException("forbidden yeah");
+        }
+
+        orderService.deleteOrderById(orderId);
     }
 
     private void updateUserEmail(User user,
