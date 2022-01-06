7cb1e9e6850b7696c8642352945c61c02da76136
Stephen <stephenbrady365@gmail.com>
2022-01-06 20:36:03 +0000

    Refactor UserDto to return Address object rather than address columns

--- src/main/java/sb/ecomm/user/dto/UserDTO.java
+++ src/main/java/sb/ecomm/user/dto/UserDTO.java
@@ -3,6 +3,7 @@ package sb.ecomm.user.dto;
 import lombok.Getter;
 import lombok.Setter;
 import sb.ecomm.order.dto.OrderDTO;
+import sb.ecomm.user.Address;
 import sb.ecomm.user.Role;
 
 import java.util.Set;
@@ -17,12 +18,5 @@ public class UserDTO {
     private String email;
     private Role role;
     private Set<OrderDTO> orders;
-
-    private String line1;
-    private String line2;
-    private String line3;
-    private String city;
-    private String province;
-    private String country;
-    private String zipCode;
+    private Address address;
 }
