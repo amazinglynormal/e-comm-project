a246660f2f04d70b15b74a8244a5b6b07393020a
Stephen <stephenbrady365@gmail.com>
2022-01-06 20:21:20 +0000

    Add embeddable Address class

---
+++ src/main/java/sb/ecomm/user/Address.java
@@ -0,0 +1,73 @@
+package sb.ecomm.user;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+    private String line1;
+    private String line2;
+    private String line3;
+    private String city;
+    private String province;
+    private String country;
+    private String zipCode;
+
+    public Address() {}
+
+    public String getLine1() {
+        return line1;
+    }
+
+    public void setLine1(String line1) {
+        this.line1 = line1;
+    }
+
+    public String getLine2() {
+        return line2;
+    }
+
+    public void setLine2(String line2) {
+        this.line2 = line2;
+    }
+
+    public String getLine3() {
+        return line3;
+    }
+
+    public void setLine3(String line3) {
+        this.line3 = line3;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
