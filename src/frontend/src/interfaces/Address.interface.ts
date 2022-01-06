08d22ae9e1ec0de7967ab92636d3d68c1981766e
Stephen <stephenbrady365@gmail.com>
2022-01-06 20:51:18 +0000

    Add Address interface

---
+++ src/frontend/src/interfaces/Address.interface.ts
@@ -0,0 +1,11 @@
+interface Address {
+  line1?: string;
+  line2?: string;
+  line3?: string;
+  city?: string;
+  province?: string;
+  country?: string;
+  zipCode?: string;
+}
+
+export default Address;
