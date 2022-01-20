64fa68b81c216acb5fb7a6359aabf4757f828cc7
Stephen Brady <stephenbrady365@gmail.com>
2022-01-20 10:39:44 +0000

    Add refresh route to AuthenticationController

--- src/main/java/sb/ecomm/auth/AuthenticationController.java
+++ src/main/java/sb/ecomm/auth/AuthenticationController.java
@@ -1,12 +1,9 @@
 package sb.ecomm.auth;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
-import sb.ecomm.security.UserDetailsImpl;
 
 import java.util.UUID;
 
@@ -14,39 +11,28 @@ import java.util.UUID;
 @RequestMapping("/api/v1/auth")
 public class AuthenticationController {
 
-    private final AuthenticationManager authenticationManager;
     private final AuthenticationService authenticationService;
 
-    public AuthenticationController(AuthenticationManager authenticationManager, AuthenticationService authenticationService) {
-        this.authenticationManager = authenticationManager;
+    public AuthenticationController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
 
     @PostMapping("/login")
-    ResponseEntity authenticateUser(@RequestBody AuthenticationRequest authenticationRequest) {
+    ResponseEntity<?> authenticateUser(@RequestBody AuthenticationRequest authenticationRequest) {
         return authenticationService.authenticateUser(authenticationRequest);
     }
 
-    @PostMapping("/{id}")
-    void reauthenticateUser(@RequestBody AuthenticationRequest authReq,
-                            @PathVariable UUID id) throws AuthenticationException {
-
-            UsernamePasswordAuthenticationToken token =
-                    new UsernamePasswordAuthenticationToken(authReq.getEmail(), authReq.getPassword());
-            Authentication authentication =
-                    authenticationManager.authenticate(token);
-
-            UserDetailsImpl user = (UserDetailsImpl) authentication.getPrincipal();
-
-            if (!authentication.isAuthenticated() ||
-                    !isCorrectUser(user.getId(), id)) {
-                throw new RuntimeException("Hit an Authentication Exception");
-            }
+    @PostMapping("/refresh")
+    ResponseEntity<?> refreshUser(@RequestHeader(name = "Authorization") String authorizationHeader,
+                               @CookieValue(name = "_Secure-fingerprint") String fingerprint) {
+        String refreshToken = authorizationHeader.split(" ")[1];
+        return authenticationService.refreshUser(refreshToken, fingerprint);
     }
 
-    private boolean isCorrectUser(UUID authenticatedId, UUID suppliedId) {
-        return authenticatedId.equals(suppliedId);
+    @PostMapping("/{userId}")
+    ResponseEntity<HttpStatus> reauthenticateUser(@RequestBody AuthenticationRequest authenticationRequest,
+                                                  @PathVariable UUID userId) throws AuthenticationException {
+
+            return authenticationService.reauthenticateUser(authenticationRequest, userId);
     }
-
-
 }
