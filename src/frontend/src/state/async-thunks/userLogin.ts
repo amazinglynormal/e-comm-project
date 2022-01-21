04e1e8be13bfbd41d9a9e932bf3b8c8e37153bd1
Stephen Brady <stephenbrady365@gmail.com>
2022-01-21 20:37:17 +0000

    Refactor to store token in session storage

--- src/frontend/src/state/async-thunks/userLogin.ts
+++ src/frontend/src/state/async-thunks/userLogin.ts
@@ -10,28 +10,38 @@ interface ThunkArgs {
   password: string;
 }
 
+interface LoginResponse {
+  refreshToken: string;
+}
+
 const userLogin = createAsyncThunk(
   "user/userLogin",
   async ({ email, password }: ThunkArgs) => {
-    const loginResponse = await axios.post("/api/v1/auth/login", {
-      email,
-      password,
-    });
+    const loginResponse = await axios.post<LoginResponse>(
+      "/api/v1/auth/login",
+      {
+        email,
+        password,
+      }
+    );
 
-    const token = extractJwtTokenFromResponseHeaders(loginResponse.headers);
+    const accessToken = extractJwtTokenFromResponseHeaders(
+      loginResponse.headers
+    );
 
-    sessionStorage.setItem("token", token);
+    sessionStorage.setItem("token", accessToken);
+    localStorage.setItem("token", loginResponse.data.refreshToken);
 
-    const decodedToken: DecodedToken = jwtDecode(token);
+    const decodedToken: DecodedToken = jwtDecode(accessToken);
 
     const getUserInfoResponse = await axios.get<User>(
       `/api/v1/users/${decodedToken.sub}`,
       {
-        headers: { Authorization: `Bearer ${token}` },
+        headers: { Authorization: `Bearer ${accessToken}` },
       }
     );
 
-    return { user: getUserInfoResponse.data, token };
+    return { user: getUserInfoResponse.data };
   }
 );
 
