f3d4b6b62c1c6d12f0b51e1562e113b50f57f5e7
Stephen <stephenbrady365@gmail.com>
2022-02-17 20:04:52 +0000

    Update CreateCheckoutSessionDTO

--- src/main/java/sb/ecomm/order/dto/CreateCheckoutSessionDTO.java
+++ src/main/java/sb/ecomm/order/dto/CreateCheckoutSessionDTO.java
@@ -3,6 +3,7 @@ package sb.ecomm.order.dto;
 import lombok.Getter;
 import lombok.Setter;
 import sb.ecomm.order.Currency;
+import sb.ecomm.user.Address;
 
 import java.util.List;
 
@@ -11,6 +12,10 @@ import java.util.List;
 public class CreateCheckoutSessionDTO {
 
     private String email;
+    private String name;
     private List<Long> productIds;
     private Currency currency;
+    private Address shippingAddress;
+    private String phone;
+
 }
