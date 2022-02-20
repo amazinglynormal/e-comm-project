4d9787301a952367d32b031dde46a369b58c7a7c
Stephen <stephenbrady365@gmail.com>
2022-02-20 09:35:19 +0000

    Refactor Order entity to allow for guest orders

--- src/main/java/sb/ecomm/order/Order.java
+++ src/main/java/sb/ecomm/order/Order.java
@@ -1,5 +1,6 @@
 package sb.ecomm.order;
 
+import sb.ecomm.user.Address;
 import sb.ecomm.user.User;
 import sb.ecomm.product.Product;
 
@@ -17,23 +18,52 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false, referencedColumnName
-            = "id", foreignKey = @ForeignKey(name = "user_order_fk"))
+    @JoinColumn(
+            name = "user_id",
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "user_order_fk")
+    )
     private User user;
 
     @OneToMany
-    @JoinColumn(name = "product_id", referencedColumnName = "id", foreignKey
-            = @ForeignKey(name = "order_product_fk"))
+    @JoinColumn(
+            name = "product_id",
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "order_product_fk")
+    )
     private List<Product> products;
+
+    @Embedded
+    private Address shippingAddress;
 
     public Order() {
     }
 
-    public Order(OrderStatus status, User user) {
+    public Order(OrderStatus status, PaymentStatus paymentStatus, User user) {
         this.status = status;
+        this.paymentStatus = paymentStatus;
         this.user = user;
     }
+
+    public Order(OrderStatus status, PaymentStatus paymentStatus, User user, List<Product> products) {
+        this.status = status;
+        this.paymentStatus = paymentStatus;
+        this.user = user;
+        this.products = products;
+    }
+
+    public Order(OrderStatus status, PaymentStatus paymentStatus, List<Product> products, Address shippingAddress) {
+        this.status = status;
+        this.paymentStatus = paymentStatus;
+        this.products = products;
+        this.shippingAddress = shippingAddress;
+    }
+
+
 
     public Long getId() {
         return id;
@@ -51,6 +81,14 @@ public class Order {
         this.status = status;
     }
 
+    public PaymentStatus getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(PaymentStatus paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
     public User getUser() {
         return user;
     }
@@ -65,5 +103,13 @@ public class Order {
 
     public void setProducts(List<Product> products) {
         this.products = products;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
     }
 }
