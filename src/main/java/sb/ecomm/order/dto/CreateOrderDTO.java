bd9b3c5ea8ed2bdce9f8dbbb77065d3c8f49f4fd
Stephen <stephenbrady365@gmail.com>
2022-02-20 09:51:43 +0000

    Refactor CreateOrderDTO to include new columns

--- src/main/java/sb/ecomm/order/dto/CreateOrderDTO.java
+++ src/main/java/sb/ecomm/order/dto/CreateOrderDTO.java
@@ -3,6 +3,7 @@ package sb.ecomm.order.dto;
 import lombok.Getter;
 import lombok.Setter;
 import sb.ecomm.order.OrderStatus;
+import sb.ecomm.user.Address;
 
 import java.util.List;
 import java.util.UUID;
@@ -14,4 +15,8 @@ public class CreateOrderDTO {
 
     private OrderStatus status;
     private List<Long> productIds;
+    private Address shippingAddress;
+    private String email;
+    private String phone;
+
 }
