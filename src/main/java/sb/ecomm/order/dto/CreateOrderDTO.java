9c046367855c08eb4a1df3525bd7e2a93aaaf77c
Stephen <stephenbrady365@gmail.com>
2022-02-22 17:29:17 +0000

    Remove status field from CreateOrderDTO

--- src/main/java/sb/ecomm/order/dto/CreateOrderDTO.java
+++ src/main/java/sb/ecomm/order/dto/CreateOrderDTO.java
@@ -2,18 +2,15 @@ package sb.ecomm.order.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import sb.ecomm.order.OrderStatus;
 import sb.ecomm.user.Address;
 
 import java.util.List;
-import java.util.UUID;
 
 
 @Getter
 @Setter
 public class CreateOrderDTO {
 
-    private OrderStatus status;
     private List<Long> productIds;
     private Address shippingAddress;
     private String email;
