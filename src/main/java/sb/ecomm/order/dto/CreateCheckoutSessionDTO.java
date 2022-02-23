fbb4f9a8b52528bdfa7dbad71ad19229854c315c
Stephen <stephenbrady365@gmail.com>
2022-02-23 08:39:08 +0000

    Remove name field from CreateCheckoutSessionDTO

--- src/main/java/sb/ecomm/order/dto/CreateCheckoutSessionDTO.java
+++ src/main/java/sb/ecomm/order/dto/CreateCheckoutSessionDTO.java
@@ -12,10 +12,10 @@ import java.util.List;
 public class CreateCheckoutSessionDTO {
 
     private String email;
-    private String name;
     private List<Long> productIds;
     private Currency currency;
     private Address shippingAddress;
     private String phone;
+    private Long orderId;
 
 }
