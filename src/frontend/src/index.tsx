f7ec31ffd688f889e6de023f966b9c7963654e8a
Stephen <stephenbrady365@gmail.com>
2022-02-27 11:37:45 +0000

    Check for existing order in localStorage on startup
    Will check localStorage for existing order on startup and
    if found will make  active order in state

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,15 +1,23 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import refresh from "./state/async-thunks/refresh";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
-import refresh from "./state/async-thunks/refresh";
+import { checkLocalStorageForExistingOrder } from "./utils/localStorageOrderUtils";
+import { setAsActiveOrder } from "./state/orderSlice";
 
 async function attemptRefresh() {
   await store.dispatch(refresh());
 }
 
 attemptRefresh();
+
+const existingOrder = checkLocalStorageForExistingOrder();
+
+if (existingOrder) {
+  store.dispatch(setAsActiveOrder(existingOrder));
+}
 
 ReactDOM.render(
   <StrictMode>
