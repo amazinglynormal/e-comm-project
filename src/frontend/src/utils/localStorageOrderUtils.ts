6b259c769f43fb4e5096a1e54827447e219fdbe5
Stephen <stephenbrady365@gmail.com>
2022-02-27 10:10:38 +0000

    Create utils for modifying orders in local storage

---
+++ src/frontend/src/utils/localStorageOrderUtils.ts
@@ -0,0 +1,64 @@
+import Order from "../interfaces/order.interface";
+import Product from "../types/Product.type";
+
+const KEY = "locallySavedOrder";
+
+function checkLocalStorageForExistingOrder(): Order | null {
+  const order = localStorage.getItem(KEY);
+
+  if (order !== null) {
+    const parsedOrder: Order = JSON.parse(order);
+    return parsedOrder;
+  }
+
+  return null;
+}
+
+function addProductToLocalStorageOrder(product: Product): Order {
+  const storedOrder = localStorage.getItem(KEY);
+  let order: Order;
+
+  if (storedOrder !== null) {
+    order = JSON.parse(storedOrder!);
+  } else {
+    order = { products: [] };
+  }
+
+  order.products.push(product);
+
+  const stringifiedOrder = JSON.stringify(order);
+
+  localStorage.setItem(KEY, stringifiedOrder);
+
+  return order;
+}
+
+function removeProductFromLocalStorageOrder(product: Product): Order {
+  const storedOrder = localStorage.getItem(KEY);
+  const order: Order = JSON.parse(storedOrder!);
+
+  const indexOfProduct = order.products.findIndex((p) => p.id === product.id);
+
+  if (indexOfProduct === -1) {
+    return order;
+  }
+
+  order.products.splice(indexOfProduct, 1);
+
+  const stringifiedOrder = JSON.stringify(order);
+
+  localStorage.setItem(KEY, stringifiedOrder);
+
+  return order;
+}
+
+function deleteOrderFromLocalStorage(): void {
+  localStorage.removeItem(KEY);
+}
+
+export {
+  checkLocalStorageForExistingOrder,
+  addProductToLocalStorageOrder,
+  removeProductFromLocalStorageOrder,
+  deleteOrderFromLocalStorage,
+};
