ad46c7c7902124cdc7573f83d1d4467e135660cc
Stephen <stephenbrady365@gmail.com>
2022-02-28 10:07:37 +0000

    Add findByStripeSessionId query to OrderRepository

--- src/main/java/sb/ecomm/order/OrderRepository.java
+++ src/main/java/sb/ecomm/order/OrderRepository.java
@@ -3,6 +3,9 @@ package sb.ecomm.order;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    Optional<Order> findOrderByStripeSessionId(String stripeSessionId);
 }
