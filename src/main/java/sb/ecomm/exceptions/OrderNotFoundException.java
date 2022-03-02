5ffc63725eca76f2713a53112ca7c595154dc7e7
Stephen <stephenbrady365@gmail.com>
2022-03-02 18:30:07 +0000

    Add no arg constructor to OrderNotFoundException

--- src/main/java/sb/ecomm/exceptions/OrderNotFoundException.java
+++ src/main/java/sb/ecomm/exceptions/OrderNotFoundException.java
@@ -4,4 +4,6 @@ public class OrderNotFoundException extends RuntimeException {
     public OrderNotFoundException(Long id) {
         super("Could not find order " + id);
     }
+
+    public OrderNotFoundException() {super("Could not find order");}
 }
