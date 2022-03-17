2a93b8d8410f6f846b65c173c52e350959a77aff
Stephen <stephenbrady365@gmail.com>
2022-03-17 17:59:08 +0000

    Add Query to select all completed order by user

--- src/main/java/sb/ecomm/order/OrderRepository.java
+++ src/main/java/sb/ecomm/order/OrderRepository.java
@@ -1,11 +1,21 @@
 package sb.ecomm.order;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
     Optional<Order> findOrderByStripePaymentIntentId(String stripePaymentIntentId);
+
+    @Query(
+            "SELECT o FROM Order o WHERE o.user.id = :userId AND o.status NOT IN (:statuses) ORDER BY o.datePlaced DESC"
+    )
+    List<Order> findUserOrders(@Param("userId") UUID userId, @Param("statuses") Collection<OrderStatus> statuses);
 }
