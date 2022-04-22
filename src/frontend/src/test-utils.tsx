144732163ebc9a61eb9cd9b20113be74500dcb32
Stephen <stephenbrady365@gmail.com>
2022-04-22 09:11:27 +0100

    Set up jest for frontend testing

---
+++ src/frontend/src/test-utils.tsx
@@ -0,0 +1,22 @@
+import { FC, ReactElement } from "react";
+import { render, RenderOptions } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./state/store";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const AllTheProviders: FC = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <Router>{children}</Router>
+    </Provider>
+  );
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from "@testing-library/react";
+
+export { customRender as render };
