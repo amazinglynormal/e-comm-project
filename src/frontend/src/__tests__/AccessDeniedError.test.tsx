1b3969c0d2dfaca74e2bf48c9874e12c9c4fec6a
Stephen <stephenbrady365@gmail.com>
2022-04-24 11:51:48 +0100

    Add test for AccessDeniedError

---
+++ src/frontend/src/__tests__/AccessDeniedError.test.tsx
@@ -0,0 +1,16 @@
+import { render } from "../test-utils";
+import AccessDeniedError from "../components/AccessDeniedError";
+
+describe("<AccessDeniedError>", () => {
+  test("renders correctly with props", () => {
+    const { getByText, getByRole } = render(
+      <AccessDeniedError message="test message" linkTo="/test-link" />
+    );
+
+    const message = getByText("test message");
+    expect(message).toBeInTheDocument();
+
+    const link = getByRole("link");
+    expect(link).toHaveAttribute("href", expect.stringMatching("/test-link"));
+  });
+});
