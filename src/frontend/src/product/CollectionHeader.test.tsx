16eb15d8529f8353052288be10d49c9a07d4bba4
Stephen <stephenbrady365@gmail.com>
2022-04-29 16:33:34 +0100

    Add tests for CollectionHeader

---
+++ src/frontend/src/product/CollectionHeader.test.tsx
@@ -0,0 +1,19 @@
+import { render } from "../test-utils";
+import CollectionHeader from "./CollectionHeader";
+
+describe("<CollectionHeader>", () => {
+  test("renders correctly with given props", () => {
+    const { getByRole, getByText } = render(
+      <CollectionHeader
+        collectionName="Test collection"
+        collectionDescription="test description"
+      />
+    );
+
+    const header = getByRole("heading");
+    expect(header).toHaveTextContent("Test collection");
+
+    const description = getByText("test description");
+    expect(description).toBeInTheDocument();
+  });
+});
