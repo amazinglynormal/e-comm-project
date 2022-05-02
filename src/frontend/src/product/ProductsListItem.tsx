10bc607da3141ecf22722978df05e4fdbd0bc813
Stephen <stephenbrady365@gmail.com>
2022-05-02 11:59:12 +0100

    Remove unneeded key attribute

--- src/frontend/src/product/ProductsListItem.tsx
+++ src/frontend/src/product/ProductsListItem.tsx
@@ -16,11 +16,7 @@ const ProductsListItem = ({ product }: Props) => {
   const { currency } = useCurrency();
 
   return (
-    <Link
-      key={product.id}
-      to={`/product/details/${product.id}`}
-      className="group"
-    >
+    <Link to={`/product/details/${product.id}`} className="group">
       <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
         <img
           src={product.imageSrc}
