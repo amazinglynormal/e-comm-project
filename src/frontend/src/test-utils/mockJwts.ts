39d59f0f5078d07f6d8da6c988306b1fb25a6f5e
Stephen <stephenbrady365@gmail.com>
2022-05-31 17:00:53 +0100

    Add mock Jwts to test-utils folder

---
+++ src/frontend/src/test-utils/mockJwts.ts
@@ -0,0 +1,7 @@
+const mockAccessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+const mockRefreshToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lMSIsImlhdCI6MTUxNjIzOTAyMn0.i1iUsfM0gbugjE0QhAP0s-R1knolQ3JFO61n_EoVz_c";
+
+export { mockAccessToken, mockRefreshToken };
