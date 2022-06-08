f5d21f29b416d5994934f7c511a5798a28bb7a84
Stephen <stephenbrady365@gmail.com>
2022-06-08 20:41:40 +0100

    Finalise CollectionSelection for Home page

--- src/frontend/src/home/CollectionSelection.tsx
+++ src/frontend/src/home/CollectionSelection.tsx
@@ -2,30 +2,25 @@ import { Link } from "react-router-dom";
 
 const callouts = [
   {
-    name: "Desk and Office",
-    description: "Work from home accessories",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg",
-    imageAlt:
-      "Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.",
-    href: "/orderhistory",
+    name: "Sunglasses",
+    description: "Shield from bright lights and awkward eye contact",
+    imageSrc: "/images/sunglasses.jpg",
+    imageAlt: "Gold-rimmed sunglasses sitting on a reflective surface.",
+    href: "/products/accessories/sunglasses",
   },
   {
-    name: "Self-Improvement",
-    description: "Journals and note-taking",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg",
-    imageAlt:
-      "Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.",
-    href: "#",
+    name: "Hoodies",
+    description: "Cosy and warm",
+    imageSrc: "/images/hoodie.jpg",
+    imageAlt: "Hooded man with his hands in his pockets looking at the ground.",
+    href: "/products/clothing/hoodies",
   },
   {
-    name: "Travel",
-    description: "Daily commute essentials",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg",
-    imageAlt: "Collection of four insulated travel bottles on wooden shelf.",
-    href: "#",
+    name: "Trainers",
+    description: "stylish and essential",
+    imageSrc: "/images/trainers.jpg",
+    imageAlt: "Out-stretched arm holding high-top trainers by the laces.",
+    href: "/products/footwear/trainers",
   },
 ];
 
