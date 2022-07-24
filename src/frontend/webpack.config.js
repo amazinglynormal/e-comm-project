3063ad8deb1b3a6d9e7eec8a2dc4d6a583b5ce60
Stephen <stephenbrady365@gmail.com>
2022-07-24 20:03:37 +0100

    Add Favicon support

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -40,6 +40,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src/index.html"),
+      favicon: "./src/favicon.ico",
     }),
     new MiniCssExtractPlugin(),
     isDevelopment && new ReactRefreshWebpackPlugin(),
