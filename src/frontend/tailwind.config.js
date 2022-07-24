70f4c53c47b366c1ea238ef2c36fcb8c8a702c76
Stephen <stephenbrady365@gmail.com>
2022-07-24 20:23:32 +0100

    Add Pacifico font to hmtl  and tailwind config

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        pacifico: ["Pacifico", "cursive"],
       },
       colors: {
         green: colors.emerald,
