244db2dab581067a6ca3159b4b5ff9617cc77a60
Stephen <stephenbrady365@gmail.com>
2022-08-07 13:23:52 +0100

    Add tests for ProductService

--- src/test/java/sb/ecomm/product/ProductServiceTest.java
+++ src/test/java/sb/ecomm/product/ProductServiceTest.java
@@ -1,11 +1,18 @@
 package sb.ecomm.product;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import sb.ecomm.category.Category;
 import sb.ecomm.category.CategoryRepository;
 import sb.ecomm.exceptions.ProductNotFoundException;
@@ -14,8 +21,10 @@ import sb.ecomm.product.dto.ProductDTO;
 import sb.ecomm.product.dto.QueryResults;
 import sb.ecomm.product.dto.UpdateProductDTO;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -29,39 +38,148 @@ class ProductServiceTest {
     @Mock
     private CategoryRepository categoryRepository;
 
-    @Mock
-    private ModelMapper mapper;
+    private ModelMapper mapper = new ModelMapper();
 
-    @InjectMocks
     private ProductService productService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        productService = new ProductService(productRepository, categoryRepository, mapper);
+    }
 
     @Test
     void findProductsByCategoriesAndColorsAndSizes() {
+        Category cat1 = getTestCategory(1L);
+        when(categoryRepository.findByName("test category 1")).thenReturn(Optional.of(cat1));
+
+        Product prod1 = getTestProduct(1L, Color.BLACK, "S", 1L);
+        Product prod2 = getTestProduct(2L, Color.BLUE, "S", 1L);
+//        Product prod3 = getTestProduct(3L, Color.BROWN, "L", 1L);
+//        Product prod4 = getTestProduct(4L, Color.BLACK, "S", 2L);
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        List<Product> products = List.of(prod1, prod2);
+        Page<Product> results = new PageImpl<>(products, pageRequest, 2);
+        when(productRepository.findProductsByCategoryInAndColorInAndSizeIn(anyList(), anyList(), anyList(), any(Pageable.class))).thenReturn(results);
+
+        List<String> colors = List.of("BLACK", "BLUE");
+        List<String> sizes = List.of("S");
+
+        QueryResults queryResults = productService.findProductsByCategoriesAndColorsAndSizes(List.of("test category 1"), colors, sizes, 1);
+
+        assertNotNull(queryResults);
+        assertEquals(2, queryResults.getTotalProducts());
+        assertEquals(1, queryResults.getTotalPages());
+        assertEquals(1L,queryResults.getProducts().get(0).getId());
+        assertEquals(2L,queryResults.getProducts().get(1).getId());
+
 
     }
 
     @Test
     void findProductsByCategories() {
+        Category cat1 = getTestCategory(1L);
+        when(categoryRepository.findByName("test category 1")).thenReturn(Optional.of(cat1));
+
+        Product prod1 = getTestProduct(1L, Color.BLACK, "S", 1L);
+        Product prod2 = getTestProduct(2L, Color.BLUE, "S", 1L);
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        List<Product> products = List.of(prod1, prod2);
+        Page<Product> results = new PageImpl<>(products, pageRequest, 2);
+        when(productRepository.findProductsByCategoryIn(anyCollection(), any(Pageable.class))).thenReturn(results);
+
+        QueryResults queryResults = productService.findProductsByCategories(List.of("test category 1"), 0);
+
+        assertNotNull(queryResults);
+        assertEquals(2, queryResults.getTotalProducts());
+        assertEquals(1, queryResults.getTotalPages());
+        assertEquals(1L,queryResults.getProducts().get(0).getId());
+        assertEquals(2L,queryResults.getProducts().get(1).getId());
     }
 
     @Test
     void findProductsByCategoriesAndColors() {
+        Category cat1 = getTestCategory(1L);
+        when(categoryRepository.findByName("test category 1")).thenReturn(Optional.of(cat1));
+
+        Product prod1 = getTestProduct(1L, Color.BLACK, "S", 1L);
+        Product prod2 = getTestProduct(2L, Color.BLUE, "S", 1L);
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        List<Product> products = List.of(prod1, prod2);
+        Page<Product> results = new PageImpl<>(products, pageRequest, 2);
+        when(productRepository.findProductsByCategoryInAndColorIn(anyList(), anyList(), any(Pageable.class))).thenReturn(results);
+
+        List<String> colors = List.of("BLACK", "BLUE");
+
+        QueryResults queryResults = productService.findProductsByCategoriesAndColors(List.of("test category 1"), colors, 0);
+
+        assertNotNull(queryResults);
+        assertEquals(2, queryResults.getTotalProducts());
+        assertEquals(1, queryResults.getTotalPages());
+        assertEquals(1L,queryResults.getProducts().get(0).getId());
+        assertEquals(2L,queryResults.getProducts().get(1).getId());
     }
 
     @Test
     void findProductsByCategoriesAndSizes() {
+        Category cat1 = getTestCategory(1L);
+        when(categoryRepository.findByName("test category 1")).thenReturn(Optional.of(cat1));
+
+        Product prod1 = getTestProduct(1L, Color.BLACK, "S", 1L);
+        Product prod2 = getTestProduct(2L, Color.BLUE, "S", 1L);
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        List<Product> products = List.of(prod1, prod2);
+        Page<Product> results = new PageImpl<>(products, pageRequest, 2);
+        when(productRepository.findProductsByCategoryInAndSizeIn(anyList(), anyList(), any(Pageable.class))).thenReturn(results);
+
+        List<String> sizes = List.of("S");
+
+        QueryResults queryResults = productService.findProductsByCategoriesAndSizes(List.of("test category 1"), sizes, 0);
+
+        assertNotNull(queryResults);
+        assertEquals(2, queryResults.getTotalProducts());
+        assertEquals(1, queryResults.getTotalPages());
+        assertEquals(1L,queryResults.getProducts().get(0).getId());
+        assertEquals(2L,queryResults.getProducts().get(1).getId());
     }
 
     @Test
     void findAlternativeSizesForProduct() {
+        Product prod1 = getTestProduct(1L, Color.BLACK, "S", 99L);
+        Product prod2 = getTestProduct(2L, Color.BLACK, "M", 99L);
+        Product prod3 = getTestProduct(3L, Color.BLACK, "L", 99L);
+        Product prod4 = getTestProduct(4L, Color.BLACK, "XL", 99L);
+
+        when(productRepository.findById(1L)).thenReturn(Optional.of(prod1));
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        List<Product> products = List.of(prod1, prod2, prod3, prod4);
+        Page<Product> results = new PageImpl<>(products, pageRequest, 4);
+
+        when(productRepository.findProductsByNameAndCategoryAndColor(eq(prod1.getName()), eq(prod1.getCategory()), eq(prod1.getColor()), any(Pageable.class))).thenReturn(results);
+
+        Iterable<ProductDTO> response = productService.findAlternativeSizesForProduct(1L);
+
+        assertNotNull(response);
+        List<ProductDTO> listProdDTOs = new ArrayList<>();
+        response.forEach(productDTO -> listProdDTOs.add(productDTO));
+        assertEquals(4, listProdDTOs.size());
+        assertEquals(2L, listProdDTOs.get(1).getId());
+        assertEquals("XL", listProdDTOs.get(3).getSize());
     }
 
     @Test
     void findProductById_success() {
         Product product = getTestProduct(1L, Color.BLACK, "S", 99L);
-        ProductDTO productDTO = getTestProductDTO(product);
         when(productRepository.findById(1L)).thenReturn(Optional.of(product));
-        when(mapper.map(product, ProductDTO.class)).thenReturn(productDTO);
 
         ProductDTO response = productService.findProductById(1L);
 
@@ -93,14 +211,9 @@ class ProductServiceTest {
 
         Product product = getTestProduct(1L, Color.BLACK, "S", 99L);
 
-        when(mapper.map(createProductDTO, Product.class)).thenReturn(product);
-        when(productRepository.save(product)).thenReturn(product);
+        when(productRepository.save(any(Product.class))).thenReturn(product);
 
         when(categoryRepository.findById(99L)).thenReturn(Optional.of(product.getCategory()));
-
-        ProductDTO productDTO = getTestProductDTO(product);
-
-        when(mapper.map(product, ProductDTO.class)).thenReturn(productDTO);
 
         ProductDTO response = productService.addNewProduct(createProductDTO);
 
@@ -127,10 +240,6 @@ class ProductServiceTest {
         product.setSize("M");
 
         when(productRepository.save(product)).thenReturn(product);
-
-        ProductDTO productDTO = getTestProductDTO(product);
-
-        when(mapper.map(product, ProductDTO.class)).thenReturn(productDTO);
 
         ProductDTO response = productService.updateProduct(1L, updateProductDTO);
 
@@ -183,5 +292,11 @@ class ProductServiceTest {
 
     private QueryResults getQueryResults(List<ProductDTO> productDTOList) {
         return new QueryResults(productDTOList, productDTOList.size(), 1);
+    }
+
+    private Category getTestCategory(Long id) {
+        Category category = new Category("test category " + id, "test description", Set.of());
+        category.setId(id);
+        return category;
     }
 }
